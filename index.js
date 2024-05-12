48249c7034a5ef23c2af5e8857c34cb15ef7e5b6
Chethan Kumar S V <53275990+checodezz@users.noreply.github.com>
2024-05-12 18:06:17 +0000

    add route to add books

--- index.js
+++ index.js
@@ -1,25 +1,62 @@
+const express = require("express");
+const app = express();
 const { initializeDatabase } = require("./db/db.connect");
-const fs = require("fs");
 const Book = require("./models/books.model");
+app.use(express.json());
 initializeDatabase();
 
-const jsonData = fs.readFileSync("books.json", "utf8");
-const booksData = JSON.parse(jsonData);
+// const jsonData = fs.readFileSync("books.json", "utf8");
+// const booksData = JSON.parse(jsonData);
 
-function seedData() {
+/* function seedData() {
   for (const bookData of booksData) {
     const newBook = new Book({
       title: bookData.title,
       author: bookData.author,
       publishedYear: bookData.publishedYear,
       genre: bookData.genre,
-      languages: bookData.languages,
+      language: bookData.language,
       country: bookData.country,
       rating: bookData.rating,
       summary: bookData.summary,
+      coverImageUrl : bookData.coverImageUrl
     });
     newBook.save();
     console.log("Books Data", newBook.title);
   }
 }
 seedData();
+ */
+
+// app.get("/", async (req, res) => {
+//   await res.send("Hello");
+// });
+
+//1. Create an API with route "/books" to create a new book data in the books Database. Make sure to do error handling. Test your API with Postman. Add the following book:
+
+async function createBook(newBook) {
+  try {
+    const book = new Book(newBook);
+    const saveBook = await book.save();
+    return saveBook;
+  } catch (error) {
+    throw error;
+  }
+}
+
+app.post("/books", async (req, res) => {
+  try {  
+    const book = await createBook(req.body);
+    if (book) {
+      res.status(201).json({ message: "book added successfully." , book : book});
+    } 
+  } catch (error) {
+    res.status(500).json({ error: "Failed to add book." });
+  }
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`App is listening at port ${PORT}`);
+});
+
